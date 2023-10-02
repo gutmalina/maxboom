f7bce81bf17913efe45c897a883211a10a523b6e
Marina Gutkevich <gutmalina@mail.ru>
2023-10-02 19:27:29 +0300

    delete hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { RouterProvider, HashRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index'
 
 function App() {
-  return <HashRouter><RouterProvider router={router} /></HashRouter>
+  return <RouterProvider router={router} />
 }
 
 export default App
