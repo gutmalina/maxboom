421b49cdb52749ae6e774d9d18d53e1ee818a00b
Marina Gutkevich <gutmalina@mail.ru>
2023-10-02 18:53:54 +0300

    add hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux'
 import store from './store/index'
 import './index.scss'
 import App from './App'
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
